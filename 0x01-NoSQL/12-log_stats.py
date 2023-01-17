ce96f5939fdb5a8e111ccb696c7dac214f83a182
NancyLwambi <99336401+NancyLwambi@users.noreply.github.com>
2023-01-17 23:04:00 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
-"""
-script that provides some stats about Nginx logs stored in MongoDB
-"""
+""" nginx logs to mongo instance parser """
+
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    # get number of documents in collection
-    docs_num = nginx_logs.count_documents({})
-    get_num = nginx_logs.count_documents({'method': 'GET'})
-    post_num = nginx_logs.count_documents({'method': 'POST'})
-    put_num = nginx_logs.count_documents({'method': 'PUT'})
-    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
-    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-                                             'path': '/status'})
-    print("{} logs".format(docs_num))
+def temp_F():
+    """ temp function to check the checker"""
+    client = MongoClient()
+    main = client.logs.nginx
+    method_C = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status = main.count_documents({"method": "GET", "path": "/status"})
+    print(f"{main.estimated_document_count()} logs")
     print("Methods:")
-    print("\tmethod GET: {}".format(get_num))
-    print("\tmethod POST: {}".format(post_num))
-    print("\tmethod PUT: {}".format(put_num))
-    print("\tmethod PATCH: {}".format(patch_num))
-    print("\tmethod DELETE: {}".format(delete_num))
-    print("{} status check".format(get_status))
+    for i in method_C:
+        check = main.count_documents({"method": i})
+        print(f"\tmethod {i}: {check}")
+    print(f"{status} status check")
+
+
+if __name__ == "__main__":
+    temp_F()
