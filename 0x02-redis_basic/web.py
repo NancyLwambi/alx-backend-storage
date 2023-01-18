b13cad0676628ef83a8084db14f76d567a68f2ab
NancyLwambi <99336401+NancyLwambi@users.noreply.github.com>
2023-01-18 15:55:48 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
-"""
-Web page caching
-"""
-
-from datetime import timedelta
-from typing import Callable
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
+from typing import Callable
 
 
-cache = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def count_requests(method: Callable) -> Callable:
-    """Counts the number of requests"""
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        cache.incr("count:{}".format(args[0]), 1)
-        return method(*args, **kwargs)
-    return wrapper
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-@count_requests
+@data_cacher
 def get_page(url: str) -> str:
-    """Fetch a web page and cache it"""
-    if not cache.exists(url):
-        res = requests.get(url)
-        cache.setex(url, timedelta(seconds=10), res.content)
-        return res.content
-    return cache.get(url)
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
